6c90a4d8d236e80961c9a5423f34b6e1ad2c5d4a
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-11-09 22:35:06 -0600

    add Documentation

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  bodyParser = require('body-parser');
 
 const app = express();
 
-let topMovies = [{title: 'Citizen Kane', year: '1941'}, {title: 'Dude, Where\'s my Car?', year: '2000'}];
+//let topMovies = [{title: 'Citizen Kane', year: '1941'}, {title: 'Dude, Where\'s my Car?', year: '2000'}];
 
 app.use(morgan('common'));
 
 app.use(express.static('public'));
 
+app.use(bodyParser.json());
+
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.send('A list of all movies in the database with title, description, genre, director, image URL, whether it’s featured or not.');
 });
+
+app.get('/movies/:title', (req, res) => {
+  res.send('Information on a single movie with the title ' + req.params.title);
+});
+
+app.get('/movies/genres/:genre', (req, res) => {
+  res.send('Description of the genre ' + req.params.genre);
+});
+
+app.get('/movies/directors/:name', (req, res) => {
+  res.send('Description of the director ' + req.params.name);
+});
+
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+  res.send('Information entered by new user');
+});
+
+app.put('/users/:username', (req, res) => {
+  let newInfo = req.body;
+  res.send('New information added by user ' + req.params.username);
+});
+
+app.post('/users/:username/add/:title', (req, res) => {
+  res.send('The movie ' + req.params.title + ' was added by ' + req.params.username);
+});
+
+app.delete('/users/:username/remove/:title', (req, res) => {
+  res.send('The movie ' + req.params.title + ' was removed by ' + req.params.username);
+});
+
+app.delete('/users/delete/:username', (req, res) => {
+  res.send('The user ' + req.params.username + ' was removed from the database.');
+});
+
 
 app.get('/', (req, res) => {
   res.send('Nice to see you! 👋')
@@ -21,6 +59,8 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Oh no! That didn\'t work! 🙈')
 });
+
+
 
 app.listen(8080, () =>
   console.log('Your app is listening on port 8080.')
