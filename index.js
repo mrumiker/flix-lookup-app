23c8a860bae70766016ca27403b62434309504e3
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-11-17 15:12:20 -0600

    add Heroku for public use

--- index.js
+++ index.js
@@ -248,7 +248,7 @@ app.use((err, req, res, next) => {
 });
 
 
-
-app.listen(8080, () =>
-console.log('Your app is listening on port 8080.')
-);
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+console.log('Your app is listening on Port ' + port);
+});
