8625554ef98962761c0ee73ffa9d432b76ae9458
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-11-17 14:20:35 -0600

    add Hashed password to new user method

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ app.get('/movies/directors/:name', passport.authenticate('jwt', { session: false
 
 //Add a user
 app.post('/users', (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
   .then((user) => {
     if (user) {
@@ -107,7 +108,7 @@ app.post('/users', (req, res) => {
     else {
       Users.create({
         Username: req.body.Username,
-        Password: req.body.Password,
+        Password: hashedPassword,
         Email: req.body.Email,
         Birthday: req.body.Birthday
       })
