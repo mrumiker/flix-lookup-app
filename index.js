94366e2c173f996a4bc4738cf4c2999dc3a8bc46
mrumiker <69427808+mrumiker@users.noreply.github.com>
2020-11-27 21:49:57 -0600

    change access

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.static('public')); //let user see the documentation page
 
 app.use(bodyParser.json());
 
-let allowedOrigins = ['http://localhost:1234', 'http://localhost:57139', *];
+let allowedOrigins = ['http://localhost:1234', 'http://localhost:*'];
 
 app.use(cors({
   origin: (origin, callback) => {
