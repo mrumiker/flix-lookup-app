9d8864ccd7c780589207afe7d875495699d0d6d4
mrumiker <69427808+mrumiker@users.noreply.github.com>
2021-03-23 22:38:46 -0500

    update Cors permissions

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.static('public')); //let user see the documentation page
 
 app.use(bodyParser.json());
 
-let allowedOrigins = ['http://localhost:1234', 'http://localhost:58173', 'http://cbu.netlify.app'];
+let allowedOrigins = ['http://localhost:1234', 'http://localhost:4200', 'http://cbu.netlify.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
